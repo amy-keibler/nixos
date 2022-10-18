c92e3173cb6ea4df5ef70cea82f823d61238d7e0
Amy Keibler <amelia.keibler@gmail.com>
2022-10-18 18:27:20 -0400

    Initial NixOS configuration port to flakes

---
+++ configuration.nix
@@ -0,0 +1,160 @@
+{ config, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    binaryCaches = [ "https://hydra.iohk.io"];
+    trustedBinaryCaches = [ "https://hydra.iohk.io" ];
+    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    autoOptimiseStore = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "amy_desktop_server"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  networking.useDHCP = false;
+  networking.interfaces.enp1s0.useDHCP = true;
+
+  networking.enableIPv6 = true;
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+
+      plasma5.enable = true;
+    };
+    windowManager = {
+      xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages : [
+          haskellPackages.xmonad-contrib
+          haskellPackages.xmonad-extras
+          haskellPackages.xmonad
+        ];
+      };
+    };
+    displayManager.defaultSession = "xfce+xmonad";
+  };
+
+  services.printing.enable = true;
+  services.printing.drivers = [
+    pkgs.gutenprint
+    pkgs.gutenprintBin
+    pkgs.hplip
+  ];
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  users.users.amy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/PY5G1vwrxu4agNvVaDixP6KlOGACxyaKwHjoZUfys jane@jane_laptop"
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    audacity
+    calibre
+    direnv
+    discord
+    emacs
+    feh
+    ffmpeg
+    firefox
+    gimp
+    git
+    htop
+    inkscape
+    ispell
+    ncdu
+    pydf
+    ripgrep
+    shutter
+    slack
+    starship
+    vim
+    vlc
+    wget
+    zola
+  ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    fira-code
+    fira-code-symbols
+    source-code-pro
+  ];
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  programs.bash.promptInit = ''
+    eval "$(starship init bash)"
+  '';
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+  services.openssh.passwordAuthentication = false;
+
+  virtualisation.containers = {
+    enable = true;
+  };
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  networking.firewall.enable = false;
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      workstation = true;
+    };
+
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
